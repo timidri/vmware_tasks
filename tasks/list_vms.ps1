6d8f7c4190b1b2540b7097110662553b482c5c88
Dimitri Tischenko <dimitri@puppet.com>
2022-03-29 00:08:33 +0200

    update list_vms

--- tasks/list_vms.ps1
+++ tasks/list_vms.ps1
@@ -1,11 +1,14 @@
 #!/usr/bin/env pwsh
-[CmdletBinding()]
-Param (
-    [Parameter(Mandatory = $True)] [String] $server,
-    [Parameter(Mandatory = $True)] [String] $username,
-    [Parameter(Mandatory = $True)] [String] $password
-)
-
-write-host hello
+$params = $input
+write-host $params.ToString()
+$json = $params | ConvertFrom-Json -AsHashtable
+write-host $json
+foreach ($h in $json.GetEnumerator() ) {
+    write-host "$($h.Name) : $($h.Value)"
+    New-Variable -Name $h.Name -Value $h.Value
+}
+write-host server: $server
+write-host username: $username
 Connect-VIServer -Server $server -Protocol https -User $username -Password $password
-Get-VM | format-table
+Get-VM | format-table | Write-Output
+write-host "hello"
