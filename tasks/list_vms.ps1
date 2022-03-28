2eab40486c958d698c5b80a8b43147d2d03c4400
Dimitri Tischenko <dimitri@puppet.com>
2022-03-29 00:38:26 +0200

    first working version

--- tasks/list_vms.ps1
+++ tasks/list_vms.ps1
@@ -1,14 +1,18 @@
 #!/usr/bin/env pwsh
 $params = $input
-write-host $params.ToString()
 $json = $params | ConvertFrom-Json -AsHashtable
-write-host $json
+# create a variable for each json key/value pair
 foreach ($h in $json.GetEnumerator() ) {
-    write-host "$($h.Name) : $($h.Value)"
+    # write-host "$($h.Name) : $($h.Value)"
     New-Variable -Name $h.Name -Value $h.Value
 }
-write-host server: $server
-write-host username: $username
-Connect-VIServer -Server $server -Protocol https -User $username -Password $password
-Get-VM | format-table | Write-Output
-write-host "hello"
+
+Connect-VIServer -Server $server -Protocol https -User $username -Password $password | Out-Null
+# print error, if any
+if ($Error -ne $null) {
+    Write-Host $Error
+    Exit 1
+}
+
+# get list of VMs
+Get-VM -Name $vm_name | format-table
