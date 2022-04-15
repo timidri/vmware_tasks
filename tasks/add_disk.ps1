493691fa75619657039c8caecf642df9c4da7106
Dimitri Tischenko <dimitri@puppet.com>
2022-04-16 01:10:11 +0200

    first working version

--- tasks/add_disk.ps1
+++ tasks/add_disk.ps1
@@ -1,4 +1,9 @@
 #!/usr/bin/env pwsh
+# Setup error handling
+Set-StrictMode -Version 3.0
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
 $params = $input
 $json = $params | ConvertFrom-Json -AsHashtable
 # create a variable for each json key/value pair
@@ -6,13 +11,14 @@ foreach ($h in $json.GetEnumerator() ) {
   New-Variable -Name $h.Name -Value $h.Value
 }
 
-write-host "Adding a disk of size $size to vm $vm_name ..."
-
 try {
-  # Connect-VIServer -Server $server -Protocol https -User $username -Password $password | Out-Null
-  New-Harddisk -VM $vm_name -CapacityGB $size
+  Write-Output "Adding a disk of size [$size] to vm [$vm_name] ..."
+  # Connect to VCenter
+  Connect-VIServer -Server $server -Protocol https -User $username -Password $password | Out-Null
+  # Create the new disk
+  Get-VM $vm_name | New-HardDisk -CapacityGB $size -StorageFormat Thin
 }
 catch {
   Write-Output "Error adding a disk:"
-  Write-Output $_
+  Write-Output $PSItem
 }
